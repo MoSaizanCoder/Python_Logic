89583820e4bd5fe01cc32387bde97d482bed60ff
MoSaizanCoder <saizanans317@gmail.com>
2026-02-15 20:03:56 +0530

    Updated Pin Verification logic

--- Logical_Program/Mini_ATM.py
+++ Logical_Program/Mini_ATM.py
@@ -1,27 +1,33 @@
 AccountBalance = 0
-
+pin = int(input("Set PIN: "))
 
 while True:
-    User_input_operation = input("Check_Balance, Deposit, Withdraw, Exit : ")
-    
-    if User_input_operation == "Check_Balance":
-        print(f"Your Account Balance is : {AccountBalance}")
-    elif User_input_operation == "Deposit":
-        Deposit = int(input("Enter Amount More Than 100 To Deposit : "))
-        if Deposit >= 100:
-            AccountBalance += Deposit
-            print(f"Deposit Successful Total Balance : {AccountBalance}")
+    pin_match = int(input("Enter Your PIN or 1 For Exit: "))
+    if pin_match == pin:
+        User_input_operation = input("Check Balance, Deposit, Withdraw, Exit : ")
+        
+        if User_input_operation.lower() == "check balance":
+            print(f"Your Account Balance is : {AccountBalance}")
+        elif User_input_operation.lower() == "deposit":
+            Deposit = int(input("Enter Amount More Than 100 To Deposit : "))
+            if Deposit >= 100:
+                AccountBalance += Deposit
+                print(f"Deposit Successful Total Balance : {AccountBalance}")
+            else:
+                print("Invalid Amount Please Enter More Than 100")
+        elif User_input_operation.lower() == "withdraw":
+            Withdraw = int(input("Enter Amount More Than 100 To Withdraw : "))
+            if Withdraw <= AccountBalance:
+                print(f"Withdraw Successfull : {Withdraw}")
+                AccountBalance -= Withdraw
+                print(f"Remaining Balance : {AccountBalance}")
+            else:
+                print("Insufficient Balance")
+        elif User_input_operation.lower() == "exit":
+            break
         else:
-            print("Invalid Amount Please Enter More Than 100")
-    elif User_input_operation == "Withdraw":
-        Withdraw = int(input("Enter Amount More Than 100 To Withdraw : "))
-        if Withdraw <= AccountBalance:
-            print(f"Withdraw Successfull : {Withdraw}")
-            AccountBalance -= Withdraw
-            print(f"Remaining Balance : {AccountBalance}")
-        else:
-            print("Insufficient Balance")
-    elif User_input_operation == "Exit":
+            print("Enter Vaild Input")
+    elif pin_match == 1:
         break
     else:
-        print("Enter Vaild Input")
+        print("Try Again Pin Invalid")
